0eaf21933e59afe168ad9fadd4e1422c8b98aca5
pistachiyoda <fujisawamai21@gmail.com>
2021-08-28 15:15:30 +0900

    子プロセスの終了に応じた終了ステータスで終了するように実装した

--- main.c
+++ main.c
@@ -205,6 +205,7 @@ int main(int argc, char **argv, char **envp)
 	int		pipe_fds[2];
 	int		first_pid;
 	int		last_pid;
+	int		status;
 
 	if (argc != 5)
 	{
@@ -228,9 +229,9 @@ int main(int argc, char **argv, char **envp)
 		error("close()", NULL, NULL);
 	if (close(pipe_fds[1]) == -1)
 		error("close()", NULL, NULL);
-	if (waitpid(first_pid, NULL, 0) < 0)
+	if (waitpid(first_pid, &status, 0) < 0)
 		error("waitpid()", NULL, NULL);
-	if (waitpid(last_pid, NULL, 0) < 0)
+	if (waitpid(last_pid, &status, 0) < 0)
 		error("waitpid()", NULL, NULL);
-    exit (0);
+	exit(WEXITSTATUS(status));
 }
