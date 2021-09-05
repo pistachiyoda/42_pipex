9b0ce56de631f3d2699327abfeacb8cdcbf6a8e8
pistachiyoda <fujisawamai21@gmail.com>
2021-09-05 14:15:33 +0900

    子プロセスのリークをチェックできるデストラクタを追加した

--- src/main.c
+++ src/main.c
@@ -2,8 +2,15 @@
 void end(void)__attribute__((destructor));
 void end(void)
 {
- int ret = system("leaks pipex &> leaks.txt");
- if (ret) printf("\e[31m!leak detected!\e[0m\n");
+	pid_t current_pid;
+	char cmd_str[50];
+
+	current_pid = getpid(); 
+
+    sprintf(cmd_str, "%s %d %s\n", "leaks", current_pid, ">> leaks.txt 2>&1");
+	// ft_putstr_fd(cmd_str, 2);
+	int ret = system(cmd_str);
+	if (ret) ft_putstr_fd("\e[31m!leak detected!\e[0m\n", 2);
 }
 
 int	main(int argc, char **argv, char **envp)
